9685f0b248a6fd0f660da273352df16a753e21fb
Tom Occhino <tomocchino@gmail.com>
2021-07-28 15:51:35 -0400

    Some refactoring of movies.js

--- pages/movies.js
+++ pages/movies.js
@@ -9,47 +9,50 @@ import API_KEY from "../src/examples/movies/API_KEY.js";
 import topMovies from "../src/examples/movies/data";
 import tmdbConfig from "../src/examples/movies/config";
 
-function getImageSrc(path) {
+function createDataSourceEntry(data) {
+  return new DataSourceEntry(data.title, data.id, data);
+}
+
+function getImagePath(path) {
   let baseURL = tmdbConfig.images.base_url;
   let size = tmdbConfig.images.poster_sizes[0];
   return baseURL + size + path;
 }
 
-let dataSource = new DataSource();
-dataSource.setMaxResults(6);
-dataSource.addEntries(
-  topMovies.map((entry) => {
-    return new DataSourceEntry(entry.title, entry.id, entry);
-  })
-);
+function getSearchPath(query) {
+  let params = new URLSearchParams({
+    page: 1,
+    langauge: "un-US",
+    include_adult: false,
+    api_key: API_KEY,
+    query: query,
+  });
+  return "https://api.themoviedb.org/3/search/movie?" + params.toString();
+}
 
-dataSource.setQueryHandler((value) => {
+async function searchForMovies(value) {
   // don't hammer the network on single character input, or should we?
   if (value.length < 2) {
     return;
   }
   console.log("GOING TO THE NETWORK FOR", value);
-  let query = encodeURIComponent(value);
-  let url = `https://api.themoviedb.org/3/search/movie?language=en-US&page=1&include_adult=false&query=${query}&api_key=${API_KEY}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      // `data` has page, results, total_pages, total_results
-      // check "../src/examples/movies/data" for shape of each `results` entry
-      dataSource.addEntries(
-        data.results.map((entry) => {
-          return new DataSourceEntry(entry.title, entry.id, entry);
-        })
-      );
-    });
-});
+  let response = await fetch(getSearchPath(value));
+  let data = await response.json();
+  let movies = data.results;
+  dataSource.addEntries(movies.map(createDataSourceEntry));
+}
+
+let dataSource = new DataSource();
+dataSource.setMaxResults(6);
+dataSource.setQueryHandler(searchForMovies);
+dataSource.addEntries(topMovies.map(createDataSourceEntry));
 
 let renderer = (entry) => {
   let data = entry.getRawData();
   let date = data.release_date;
   let year = date ? new Date(data.release_date).getFullYear() : "unknown";
   let poster = data.poster_path ? (
-    <img src={getImageSrc(data.poster_path)} />
+    <img src={getImagePath(data.poster_path)} />
   ) : null;
   return (
     <span className={styles.Entry}>
