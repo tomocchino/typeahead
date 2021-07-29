6198ae932a30338bd0434c7311ad8d90894cb6c9
Tom Occhino <tomocchino@gmail.com>
2021-07-29 14:17:31 -0400

    Add a slightly better MovieDetails panel

--- pages/movies.js
+++ pages/movies.js
@@ -10,13 +10,15 @@ import API_KEY from "../src/examples/movies/API_KEY.js";
 import topMovies from "../src/examples/movies/data";
 import tmdbConfig from "../src/examples/movies/config";
 
+// Utilities
+
 function createDataSourceEntry(data) {
   return new DataSourceEntry(data.title, data.id, data);
 }
 
-function getImagePath(path) {
+function getImagePath(path, sizeIndex) {
   let baseURL = tmdbConfig.images.base_url;
-  let size = tmdbConfig.images.poster_sizes[0];
+  let size = tmdbConfig.images.poster_sizes[sizeIndex || 0];
   return baseURL + size + path;
 }
 
@@ -43,31 +45,62 @@ async function searchForMovies(value) {
   dataSource.addEntries(movies.map(createDataSourceEntry));
 }
 
+// Create DataSource
+
 let dataSource = new DataSource();
 dataSource.setMaxResults(6);
 dataSource.setQueryHandler(searchForMovies);
 dataSource.addEntries(topMovies.map(createDataSourceEntry));
 
+// Typeahead Entry Renderer
+
 let renderer = (movie) => {
   let data = movie.getRawData();
   let date = data.release_date;
-  let year = date ? new Date(data.release_date).getFullYear() : "unknown";
+  let year = date ? new Date(date).getFullYear() : "unknown";
   let poster = data.poster_path ? (
     <img src={getImagePath(data.poster_path)} />
   ) : null;
   return (
     <span className={styles.Entry}>
-      <span className={styles.Poster}>{poster}</span>
-      <span className={styles.Text}>
-        <span className={styles.Title}>{data.title}</span>
-        <span className={styles.Year}>{year}</span>
+      <span className={styles.Entry_poster}>{poster}</span>
+      <span className={styles.Entry_text}>
+        <span className={styles.Entry_title}>{data.title}</span>
+        <span className={styles.Entry_year}>{year}</span>
       </span>
     </span>
   );
 };
 
+// Movie Details Panel
+
 function MovieDetails(props) {
-  return renderer(props.movie);
+  let movie = props.movie;
+  let data = movie.getRawData();
+  let date = data.release_date;
+  let year = date ? new Date(date).getFullYear() : "unknown";
+  return (
+    <div className={styles.Details}>
+      <div>
+        <div className={styles.Details_poster}>
+          {data.poster_path ? (
+            <img src={getImagePath(data.poster_path, 2)} />
+          ) : null}
+        </div>
+      </div>
+      <div className={styles.Details_info}>
+        <div className={styles.Details_title}>
+          {data.title} <span className={styles.Details_year}>({year})</span>
+        </div>
+        {data.overview ? (
+          <p className={styles.Details_overview}>{data.overview}</p>
+        ) : null}
+        {data.vote_average ? (
+          <p className={styles.Details_rating}>Score: {data.vote_average}</p>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default function Movies() {
