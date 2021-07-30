58e91b712dcb68eefdae2ac8c32ad3948e1101fe
Tom Occhino <tomocchino@gmail.com>
2021-07-30 12:57:10 -0400

    Some cleanup / moving things around in DataSource tests

--- src/modules/DataSource.test.js
+++ src/modules/DataSource.test.js
@@ -12,77 +12,6 @@ function createTestDataSource(entries, maxResults) {
   return dataSource;
 }
 
-describe("Lake Tahoe tests", () => {
-  let dataSource = createTestDataSource([
-    "Lake Tahoe",
-    "Lake Tahoe",
-    "South Lake Tahoe",
-    "Tahoe",
-  ]);
-
-  it("should not allow duplicate entries to be added (query = 'lake tahoe')", () => {
-    let results = dataSource.getQueryResults("lake tahoe");
-    expect(results).toHaveLength(2); // Lake Tahoe, South Lake Tahoe
-  });
-
-  it("should return entries with last word matching the query (query = 'tahoe')", () => {
-    let results = flatten(dataSource.getQueryResults("tahoe"));
-    expect(results).toEqual(["Tahoe", "Lake Tahoe", "South Lake Tahoe"]);
-  });
-
-  it("should return entries with middle word matching the query (query = 'lake')", () => {
-    let results = flatten(dataSource.getQueryResults("lake"));
-    expect(results).toEqual(["Lake Tahoe", "South Lake Tahoe"]);
-  });
-
-  it("should return entries with two words matching the query (query = 'lake tahoe')", () => {
-    let results = flatten(dataSource.getQueryResults("lake tahoe"));
-    expect(results).toEqual(["Lake Tahoe", "South Lake Tahoe"]);
-  });
-
-  it("should return only entries that have a prefix match (query = 'ake')", () => {
-    let results = dataSource.getQueryResults("ake");
-    expect(results).toHaveLength(0);
-  });
-});
-
-describe("Buffalo Creek tests", () => {
-  let dataSource = createTestDataSource([
-    "Buffalo Creek",
-    "Chicago Creek",
-    "Chicago",
-    "Creek Town",
-    "Creekside",
-  ]);
-
-  it("should match the correct number of words (query = 'c c')", () => {
-    let results = flatten(dataSource.getQueryResults("c c"));
-    expect(results).toContain("Chicago Creek");
-    expect(results).not.toContain("Chicago");
-    expect(results).not.toContain("Creekside");
-  });
-
-  it("should return single word matches first (query = 'c')", () => {
-    let results = flatten(dataSource.getQueryResults("c"));
-    expect(results).toEqual([
-      "Chicago",
-      "Creekside",
-      "Chicago Creek",
-      "Creek Town",
-    ]);
-  });
-
-  it("should return single word matches first (query = 'creek')", () => {
-    let results = flatten(dataSource.getQueryResults("creek"));
-    expect(results).toEqual([
-      "Creekside",
-      "Chicago Creek",
-      "Creek Town",
-      "Buffalo Creek",
-    ]);
-  });
-});
-
 describe("Alpha Bravo tests", () => {
   let dataSource = createTestDataSource([
     "Alpha Bravo Charlie",
@@ -125,24 +54,78 @@ describe("Alpha Bravo tests", () => {
   });
 });
 
-describe("The Titan movie tests", () => {
-  // Before 1c4dbef, this would fail because there were multiple 'The Titan'
-  // entries in the 't' entryBucket, causing the count to be off. It took so
-  // much debugging to figure that out, sigh.
+describe("Buffalo Creek tests", () => {
   let dataSource = createTestDataSource([
-    "Titan",
-    "The Titan",
-    "Two Titans",
-    "Attack on Titan II: The Roar of Awakening Some Long Thing",
+    "Buffalo Creek",
+    "Chicago Creek",
+    "Chicago",
+    "Creek Town",
+    "Creekside",
   ]);
 
-  it("should return all matching entries for the query 'titan'", () => {
-    let results = dataSource.getQueryResults("titan");
-    expect(results).toHaveLength(4);
+  it("should match the correct number of words (query = 'c c')", () => {
+    let results = flatten(dataSource.getQueryResults("c c"));
+    expect(results).toContain("Chicago Creek");
+    expect(results).not.toContain("Chicago");
+    expect(results).not.toContain("Creekside");
+  });
+
+  it("should return single word matches first (query = 'c')", () => {
+    let results = flatten(dataSource.getQueryResults("c"));
+    expect(results).toEqual([
+      "Chicago",
+      "Creekside",
+      "Chicago Creek",
+      "Creek Town",
+    ]);
+  });
+
+  it("should return single word matches first (query = 'creek')", () => {
+    let results = flatten(dataSource.getQueryResults("creek"));
+    expect(results).toEqual([
+      "Creekside",
+      "Chicago Creek",
+      "Creek Town",
+      "Buffalo Creek",
+    ]);
   });
 });
 
-describe("Duplicate entry tests", () => {
+describe("Lake Tahoe tests", () => {
+  let dataSource = createTestDataSource([
+    "Lake Tahoe",
+    "Lake Tahoe",
+    "South Lake Tahoe",
+    "Tahoe",
+  ]);
+
+  it("should not allow duplicate entries to be added (query = 'lake tahoe')", () => {
+    let results = dataSource.getQueryResults("lake tahoe");
+    expect(results).toHaveLength(2); // Lake Tahoe, South Lake Tahoe
+  });
+
+  it("should return entries with last word matching the query (query = 'tahoe')", () => {
+    let results = flatten(dataSource.getQueryResults("tahoe"));
+    expect(results).toEqual(["Tahoe", "Lake Tahoe", "South Lake Tahoe"]);
+  });
+
+  it("should return entries with middle word matching the query (query = 'lake')", () => {
+    let results = flatten(dataSource.getQueryResults("lake"));
+    expect(results).toEqual(["Lake Tahoe", "South Lake Tahoe"]);
+  });
+
+  it("should return entries with two words matching the query (query = 'lake tahoe')", () => {
+    let results = flatten(dataSource.getQueryResults("lake tahoe"));
+    expect(results).toEqual(["Lake Tahoe", "South Lake Tahoe"]);
+  });
+
+  it("should return only entries that have a prefix match (query = 'ake')", () => {
+    let results = dataSource.getQueryResults("ake");
+    expect(results).toHaveLength(0);
+  });
+});
+
+describe("Duplicates tests", () => {
   let dataSource = new DataSource();
   dataSource.addEntries([
     new DataSourceEntry("A Star Is Born", "asib2018"),
@@ -199,5 +182,22 @@ describe("Keywords tests", () => {
   it("should return text matches before keyword matches (query = 'f')", () => {
     let results = flatten(dataSource.getQueryResults("f"));
     expect(results).toEqual(["four", "five", "one"]); // 'one' matches on keyword 'first'
+  });
+});
+
+describe("Titan Movie tests", () => {
+  // Before 1c4dbef, this would fail because there were multiple 'The Titan'
+  // entries in the 't' entryBucket, causing the count to be off. It took so
+  // much debugging to figure that out, sigh.
+  let dataSource = createTestDataSource([
+    "Titan",
+    "The Titan",
+    "Two Titans",
+    "Attack on Titan II: The Roar of Awakening Some Long Thing",
+  ]);
+
+  it("should return all matching entries for the query 'titan'", () => {
+    let results = dataSource.getQueryResults("titan");
+    expect(results).toHaveLength(4);
   });
 });
