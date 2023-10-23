d6af145083bcfb9a5121ebeb069ba221785732b9
Tom Occhino <tomocchino@gmail.com>
2023-10-22 19:13:19 -0700

    update to latest tmbd instructions using new api access

--- pages/api/movies/search.ts
+++ pages/api/movies/search.ts
@@ -1,15 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import fetch from "node-fetch";
 
 function getSearchPath(query: string) {
   let params = new URLSearchParams({
     page: "1",
     language: "en-US",
     include_adult: "false",
-    api_key: process.env.API_KEY || "",
     query: query,
   });
 
   return "https://api.themoviedb.org/3/search/movie?" + params.toString();
+}
+
+function getOptions() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + process.env.ACCESS_TOKEN || "",
+    },
+  };
+  return options;
 }
 
 export default async function handler(
@@ -19,8 +30,9 @@ export default async function handler(
   if (typeof request.url === "string") {
     let url = new URL(request.url, `https://${request.headers.host}`);
     let query = url.searchParams.get("query") ?? "";
+    let options = getOptions();
     try {
-      let data = await fetch(getSearchPath(query));
+      let data = await fetch(getSearchPath(query), options);
       response.status(200).send(data.body);
     } catch (error) {
       response.status(500).send(error);
